70b954474522f10ba25f0d6a8f9e58f1aa33ac54
nefiu <rpast@protonmail.com>
2020-06-29 00:25:29 +0200

    Gallery switcher script fixes and comments

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 //TODO: switch to class toggling
 
-// Const & Vars
+// Const & Vars //
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 const desktopW = 1024;
 
-// Toggle between adding and removing the "responsive" 
-// class to topnav when the user clicks on the icon
+// Toggle between adding and removing the "responsive" //
+// class to topnav when the user clicks on the icon //
 
 var menu = document.querySelector("#menu-wrapper");
 var nav = document.querySelector("#navbar");
@@ -31,62 +31,10 @@ function hamburgerToggle() {
         icon.classList.remove('hamburger-on');
         icon.classList.add('hamburger-off');
     }
-}
+};
 
-// Carousel handlers
-var slideIndex = 1;
-var slideIndexTeam = 1;
-
-var carouselTimer = setInterval(plusDivsInterval, 6000);
-var carouselTimerTeam = setInterval(plusDivsIntervalTeam, 6000);
-
-showDivs(slideIndex, "myslides", 1, desktopW);
-showDivs(slideIndexTeam, "myslides-team", 2, false);
-
-function plusDivsInterval() {
-	showDivs((slideIndex += 1), "myslides", 1, desktopW);
-}
-function plusDivsIntervalTeam() {
-	showDivs((slideIndexTeam += 1), "myslides-team", 2, false);
-}
-
-function plusDivs(n) {
-	showDivs((slideIndex += n), "myslides", 1, desktopW);
-	clearInterval(carouselTimer);
-}
-function plusDivsTeam(n) {
-	showDivs((slideIndexTeam += n), "myslides-team", 2, false);
-	clearInterval(carouselTimerTeam);
-}
-
-function showDivs(n, className, counterFlag, maxVw) {
-    //a simple picture switcher
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    console.log(vw)
-    if(vw < maxVw || maxVw == false) {    
-        var i;
-        var x = document.getElementsByClassName(className);
-        if (n > x.length) { //if x
-            if (counterFlag == 1) {
-                slideIndex = 1;
-            } else {
-                slideIndexTeam = 1
-            }
-        }
-
-        for (i = 0; i < x.length; i++) {
-            x[i].classList.add('off');
-            x[i].classList.remove('on')
-        }
-        if (counterFlag == 1) {
-            x[slideIndex - 1].classList.add('on');
-        } else {
-            x[slideIndexTeam - 1].classList.add('on');
-        }
-    } 
-}
-
-// Style toggler for navbar
+// Style toggler for navbar //
+//TODO: refactor variables
 var myNav = document.querySelector(".navbar");
 var myLogo = document.querySelector(".logo");
 var myLinks = document.querySelector(".navbar-links")
@@ -116,12 +64,90 @@ window.onscroll = function () {
     }
 };
 
-// Smooth scroll handler
+// Carousel handlers //
+
+//floor counter values
+var slideIndex = 1; 
+var slideIndexTeam = 1;
+
+//interval functions -> used to instantiate timer 
+function plusDivsInterval() {
+    showDivs((slideIndex += 1), "myslides", 1, desktopW);
+}
+function plusDivsIntervalTeam() {
+    showDivs((slideIndexTeam += 1), "myslides-team", 2, false);
+}
+
+//onclick functions
+function plusDivs(n) {
+    showDivs((slideIndex += n), "myslides", 1, desktopW);
+    clearInterval(carouselTimer); //stop timer
+}
+function plusDivsTeam(n) {
+    showDivs((slideIndexTeam += n), "myslides-team", 2, false);
+    clearInterval(carouselTimerTeam); //stop timer
+}
+
+function showDivs(counter, className, counterFlag, maxVw) {
+    //Function overview:
+        //a simple gallery switcher
+        //counter: a counter increment used as the item index value in the list of all elements with className
+        //className: define what elements you want to grab
+        //counterFlag: 1 or 2; used to reset the counter for different switcher instances
+        //maxVW: int or none if you dont want to limit the switcher behavior
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0); //calculate the viewport width
+
+    var i;
+    var elemList = document.getElementsByClassName(className);
+    console.log('elemList length '+elemList.length)
+    
+    if(vw < maxVw || maxVw == false) {
+        if (counter > elemList.length) { //if # of elements in the list is shorter than counter val -> reset the counter
+            if (counterFlag == 1) { //1 for office gallery
+                slideIndex = 1;
+            } else { //2 for team gallery
+                slideIndexTeam = 1
+            }
+        }
+
+        //Toggle classes on list elements
+        for (i = 0; i < elemList.length; i++) {
+            elemList[i].classList.add('off');
+            elemList[i].classList.remove('on-col')
+        }
+        if (counterFlag == 1) { //always keep counter-1 element switched on
+            elemList[slideIndex - 1].classList.add('on-col');
+        } else {
+            elemList[slideIndexTeam - 1].classList.add('on-col');
+        }
+    } else { //in case vw > threshold switch on all elements from the office gallery (design specific)
+        if(counterFlag == 1) {
+            for (i = 0; i < elemList.length; i++) {
+                elemList[i].classList.add('on-col');
+                elemList[i].classList.remove('off')
+            }
+        }
+    }
+};
+
+//instantiate the galleries and timers
+showDivs(slideIndex, "myslides", 1, desktopW);
+showDivs(slideIndexTeam, "myslides-team", 2, false);
+setInterval(plusDivsInterval, 6000);
+setInterval(plusDivsIntervalTeam, 6000);
+
+//Reload the page when viewport gets resized
+window.onresize = function(){ 
+    location.reload(); 
+    showDivs(slideIndex, "myslides", 1, desktopW); //if vw < maxVw display all office gallery elements
+}
+
+// Smooth scroll handler //     
 const scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 300,
 });
 
-//Intersection observer for catching the scroll position
+//Intersection observer for catching the scroll position //
 //TODO: implement intersection observer for lazy imgs and nav styling
 //set empty containers to observe at the begining and the end of
 //services section
