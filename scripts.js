f2b39a8042d68b02333bb23e13a780589fe3697d
nefiu <rpast@protonmail.com>
2020-06-28 22:53:26 +0200

    Carousel script and css modified

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+//TODO: switch to class toggling
+
+// Const & Vars
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+const desktopW = 1024;
+
 // Toggle between adding and removing the "responsive" 
 // class to topnav when the user clicks on the icon
 
@@ -33,44 +40,50 @@ var slideIndexTeam = 1;
 var carouselTimer = setInterval(plusDivsInterval, 6000);
 var carouselTimerTeam = setInterval(plusDivsIntervalTeam, 6000);
 
-showDivs(slideIndex, "myslides", 1);
-showDivs(slideIndexTeam, "myslides-team", 2);
+showDivs(slideIndex, "myslides", 1, desktopW);
+showDivs(slideIndexTeam, "myslides-team", 2, false);
 
 function plusDivsInterval() {
-	showDivs((slideIndex += 1), "myslides", 1);
+	showDivs((slideIndex += 1), "myslides", 1, desktopW);
 }
 function plusDivsIntervalTeam() {
-	showDivs((slideIndexTeam += 1), "myslides-team", 2);
+	showDivs((slideIndexTeam += 1), "myslides-team", 2, false);
 }
 
 function plusDivs(n) {
-	showDivs((slideIndex += n), "myslides", 1);
+	showDivs((slideIndex += n), "myslides", 1, desktopW);
 	clearInterval(carouselTimer);
 }
 function plusDivsTeam(n) {
-	showDivs((slideIndexTeam += n), "myslides-team", 2);
+	showDivs((slideIndexTeam += n), "myslides-team", 2, false);
 	clearInterval(carouselTimerTeam);
 }
 
-function showDivs(n, className, counterFlag) {
-	var i;
-    var x = document.getElementsByClassName(className);
-	if (n > x.length) {
-        if (counterFlag == 1) {
-            slideIndex = 1;
-        } else {
-            slideIndexTeam = 1
+function showDivs(n, className, counterFlag, maxVw) {
+    //a simple picture switcher
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    console.log(vw)
+    if(vw < maxVw || maxVw == false) {    
+        var i;
+        var x = document.getElementsByClassName(className);
+        if (n > x.length) { //if x
+            if (counterFlag == 1) {
+                slideIndex = 1;
+            } else {
+                slideIndexTeam = 1
+            }
         }
-	}
 
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";
-    }
-    if (counterFlag == 1) {
-        x[slideIndex - 1].style.display = "flex";
-    } else {
-        x[slideIndexTeam - 1].style.display = "flex";
-    }
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.add('off');
+            x[i].classList.remove('on')
+        }
+        if (counterFlag == 1) {
+            x[slideIndex - 1].classList.add('on');
+        } else {
+            x[slideIndexTeam - 1].classList.add('on');
+        }
+    } 
 }
 
 // Style toggler for navbar
